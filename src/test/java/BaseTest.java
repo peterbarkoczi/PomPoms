6c8ff1febf0f6f71ae4bd067a5a68e1e6fda69ce
markligeti <ligetimark.e@gmail.com>
2019-12-09 11:53:43 +0100

    BaseTest class created with BeforeEach and AfterEach

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,27 @@
+import Utils.Util;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseTest {
+    WebDriver driver;
+
+    @BeforeEach
+    void setDriver() {
+        System.setProperty("webdriver.chrome.driver", Util.getDriverPath("chromedriver"));
+        this.driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void quitDriver() {
+        driver.quit();
+    }
+
+    @Test
+    void test() {
+        driver.get("https://www.google.com");
+    }
+
 }
