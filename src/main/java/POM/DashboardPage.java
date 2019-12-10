4fa63bb08836cb172e288ec4fa21bfb0507a4c03
markligeti <ligetimark.e@gmail.com>
2019-12-10 10:32:57 +0100

    login test refactor

--- src/main/java/POM/DashboardPage.java
+++ src/main/java/POM/DashboardPage.java
@@ -4,15 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
-
 public class DashboardPage extends Page {
 
     @FindBy(id = "header-details-user-fullname")
     private WebElement avatar;
-
-    @FindBy(id = "log_out")
-    private List<WebElement> logoutButtons;
 
     public DashboardPage(WebDriver driver) {
         super(driver);
