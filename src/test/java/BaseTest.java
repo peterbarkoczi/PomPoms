5be8790e1ce2e9f44440a0334df9c0da4169cbcb
peterbarkoczi <barkopetya@gmail.com>
2019-12-10 09:57:49 +0100

    parametrized login test implemented

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,17 +1,29 @@
 import Utils.Util;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+@TestInstance(Lifecycle.PER_CLASS)
 public class BaseTest {
+
     WebDriver driver;
+    String baseUrl;
+    String username;
+    String password;
+
+    @BeforeAll
+    void setupTestEnvironment() {
+        this.baseUrl = "https://jira.codecool.codecanvas.hu/";
+        this.username = System.getenv("JIRA_USERNAME");
+        this.password = System.getenv("JIRA_PASSWORD");
+    }
 
     @BeforeEach
     void setDriver() {
         System.setProperty("webdriver.chrome.driver", Util.getDriverPath("chromedriver"));
         this.driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterEach
@@ -22,6 +34,10 @@ public class BaseTest {
     @Test
     void test() {
         driver.get("https://www.google.com");
+    }
+
+    void navigateTo(String url) {
+        driver.get(url);
     }
 
 }
